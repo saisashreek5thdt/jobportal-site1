bc625a6ec59160059261af9e0a8afbc8fb371954
Sai Sashreek D <saisashreekd@gmail.com>
2025-03-20 15:49:41 +0530

    Project Setup is Ready

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./Home";
+
+export default function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />,
+    },
+  ]);
+
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
+}
