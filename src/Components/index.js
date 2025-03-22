1f6a4562ded1ff86392ec0c8ef16bf53a092cd4b
Sai Sashreek D <saisashreekd@gmail.com>
2025-03-22 12:37:39 +0530

    Added and Updated New Page About Component

--- src/Components/index.js
+++ src/Components/index.js
@@ -1,6 +1,7 @@
 export { default as Topbar } from "./Common/Topbar";
 export { default as Header } from "./Common/Header";
 export { default as Footer } from "./Common/Footer";
+export { default as PageHeader } from "./Common/PageHeader";
 
 export { default as HeroSection } from "./Home/HeroSection";
 export { default as HeroCTA } from "./Home/HeroCTA";
@@ -8,3 +9,5 @@ export { default as About } from "./Home/About";
 export { default as OurServices } from "./Home/OurServices";
 export { default as OurFeatures } from "./Home/OurFeatures";
 export { default as OurGoals } from "./Home/OurGoals";
+
+export { default as AboutContent } from "./About/AboutContent";
